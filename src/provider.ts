2bbe92a7cf8fbef7b3f83d88513ae6cef55850de
Ali Azam <Ali_1632@hotmail.co.uk>
2019-01-02 18:10:02 +0000

    Add validation for provider config (#34)
    * Add validation for provider config

--- src/provider.ts
+++ src/provider.ts
@@ -2,22 +2,51 @@ import Ganache from "ganache-core";
 import { ethers } from "ethers";
 import { IConfig } from "./dataEntities/config";
 const config = require("./config.json") as IConfig;
+
 // provide the ability to get different providers
+
 export const getGanacheProvider = () => {
-    const ganache = Ganache.provider({
-        mnemonic: "myth like bonus scare over problem client lizard pioneer submit female collect"
-    });
-    const ganacheProvider = new ethers.providers.Web3Provider(ganache);
-    ganacheProvider.pollingInterval = 100;
-    return ganache;
+  const ganache = Ganache.provider({
+    mnemonic:
+      "myth like bonus scare over problem client lizard pioneer submit female collect"
+  });
+  const ganacheProvider = new ethers.providers.Web3Provider(ganache);
+  ganacheProvider.pollingInterval = 100;
+  validateProvider(ganacheProvider);
+  return ganache;
 };
 
+export const getJsonRPCProvider = () => {
+  const provider = new ethers.providers.JsonRpcProvider(config.jsonRpcUrl);
+  validateProvider(provider);
+  return provider;
+};
 
 export const getInfuraProvider = (): ethers.providers.InfuraProvider => {
+  const infura: any = config.infura;
+  const infuraProvider = new ethers.providers.InfuraProvider(
+    config.infura.currentNetwork,
+    infura[`${config.infura.currentNetwork}`].apikey
+  );
 
-    const infura: any = config.infura;
+  validateProvider(infuraProvider);
 
-    const infuraProvider = new ethers.providers.InfuraProvider(config.infura.currentNetwork, infura[`${config.infura.currentNetwork}`].apikey)
+  return infuraProvider;
+};
 
-    return infuraProvider
+export async function validateProvider(provider: ethers.providers.Provider) {
+  try {
+    /* if the provider is working then a valid response of a number will be returned
+            otherwise, an error will be thrown such as invalid JSON response "" which indicates 
+            the connection failed, the error will be caught here and a separate error will be thrown.
+            The address is a random valid address taken from ethersjs documentation
+      */
+    await provider.getTransactionCount(
+      "0xD115BFFAbbdd893A6f7ceA402e7338643Ced44a6"
+    );
+  } catch (err) {
+    throw new Error(
+      `Provider ${JSON.stringify(provider)} failed to connect: ${err}`
+    );
+  }
 }
--- src/startUp.ts
+++ src/startUp.ts
@@ -3,40 +3,47 @@ import { ethers } from "ethers";
 import { IConfig } from "./dataEntities/config";
 import { KitsuneWatcher } from "./watcher";
 import { KitsuneInspector } from "./inspector";
+import { getJsonRPCProvider } from "./provider";
 const config = require("./config.json") as IConfig;
-const provider = new ethers.providers.JsonRpcProvider(config.jsonRpcUrl);
-provider.pollingInterval = 100;
 
+const provider = getJsonRPCProvider();
+
+provider.pollingInterval = 100;
 
 const watcherWallet = new ethers.Wallet(config.watcherKey, provider);
 const watcher = new KitsuneWatcher(provider, watcherWallet);
 const inspector = new KitsuneInspector(10, provider);
 
 // start the pisa service
-const service = new PisaService(config.host.name, config.host.port, inspector, watcher);
+const service = new PisaService(
+  config.host.name,
+  config.host.port,
+  inspector,
+  watcher
+);
 
 // wait for a stop signal
-waitForStop();
+waitForStop(service);
 
-function waitForStop() {
-    const stdin = process.stdin;
+function waitForStop(service: PisaService) {
+  const stdin = process.stdin;
 
-    // without this, we would only get streams once enter is pressed
-    stdin.setRawMode(true);
+  // without this, we would only get streams once enter is pressed
+  stdin.setRawMode(true);
 
-    // resume stdin in the parent process (node app won't quit all by itself
-    // unless an error or process.exit() happens)
-    stdin.resume();
-    stdin.setEncoding("utf8");
-    stdin.on("data", key => {
-        // ctrl-c ( end of text )
-        if (key === "\u0003") {
-            // stop the pisa service
-            service.stop()
-            // exit the process
-            process.exit();
-        }
-        // otherwise write the key to stdout all normal like
-        process.stdout.write(key);
-    });
+  // resume stdin in the parent process (node app won't quit all by itself
+  // unless an error or process.exit() happens)
+  stdin.resume();
+  stdin.setEncoding("utf8");
+  stdin.on("data", key => {
+    // ctrl-c ( end of text )
+    if (key === "\u0003") {
+      // stop the pisa service
+      service.stop();
+      // exit the process
+      process.exit();
+    }
+    // otherwise write the key to stdout all normal like
+    process.stdout.write(key);
+  });
 }
