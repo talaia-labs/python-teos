e7ed7f8bd084beac09a8818adec77c2d18593667
Chris Buckland <cpbuckland88@gmail.com>
2019-01-03 11:49:47 +0000

    Added multistage docker file, and changed docker CMD to use the js files rather than ts

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10.14.2
+FROM node:10.14.2 as builder
 # set a location
 WORKDIR /usr/pisa
 
@@ -11,12 +11,28 @@ COPY package*.json ./
 RUN ["npm", "ci"];
 # copy the src and the tsconfig
 COPY ./src ./src
+COPY ./config.json ./config.json
 COPY ./tsconfig.json ./tsconfig.json
 # build
 RUN ["npm", "run", "build"]
 
+
+# start a new stage, we dont need to carry over all the unused precompiled code and dev dependencies
+FROM node:10.14.2
+WORKDIR /usr/pisa
+# copy packages
+COPY package*.json ./
+COPY ./config.json ./config.json
+COPY ./statechannels/build ./statechannels/build
+# install production dependencies
+RUN ["npm", "ci", "--only=prod"];
+
+
+# copy the source code from the builder
+COPY --from=builder ./usr/pisa/build ./build
+
+
 # expose the startup port
 EXPOSE 3000
-
 # start the application
-CMD ["npm", "run", "dev"]
+CMD ["npm", "run", "start"]
--- src/__docker_tests__/quickTest.ts
+++ src/__docker_tests__/quickTest.ts
@@ -1,5 +1,3 @@
-import * as chai from "chai";
-import "mocha";
 import request from "request-promise";
 import { KitsuneTools } from "../kitsuneTools";
 import { ethers } from "ethers";
@@ -9,73 +7,66 @@ const config = require("./../../config.json") as IConfig;
 const provider = getJsonRPCProvider();
 provider.pollingInterval = 100;
 
-describe("Docker smoke test!", () => {
-    let account0: string,
-        account1: string,
-        channelContract: ethers.Contract,
-        hashState: string,
-        disputePeriod: number;
+let account0: string, account1: string, channelContract: ethers.Contract, hashState: string, disputePeriod: number;
 
-    beforeEach(async () => {
-        // accounts
-        const accounts = await provider.listAccounts();
-        account0 = accounts[0];
-        account1 = accounts[1];
+let setup = async () => {
+    // accounts
+    const accounts = await provider.listAccounts();
+    account0 = accounts[0];
+    account1 = accounts[1];
 
-        // set the dispute period
-        disputePeriod = 10;
+    // set the dispute period
+    disputePeriod = 15;
 
-        // contract
-        const channelContractFactory = new ethers.ContractFactory(
-            KitsuneTools.ContractAbi,
-            KitsuneTools.ContractBytecode,
-            provider.getSigner()
-        );
-        channelContract = await channelContractFactory.deploy([account0, account1], disputePeriod);
-        hashState = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("face-off"));
+    // contract
+    const channelContractFactory = new ethers.ContractFactory(
+        KitsuneTools.ContractAbi,
+        KitsuneTools.ContractBytecode,
+        provider.getSigner()
+    );
+    channelContract = await channelContractFactory.deploy([account0, account1], disputePeriod);
+    hashState = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("face-off"));
+};
+
+let execute = async () => {
+    const round = 1,
+        setStateHash = KitsuneTools.hashForSetState(hashState, round, channelContract.address),
+        sig0 = await provider.getSigner(account0).signMessage(ethers.utils.arrayify(setStateHash)),
+        sig1 = await provider.getSigner(account1).signMessage(ethers.utils.arrayify(setStateHash)),
+        expiryPeriod = disputePeriod + 1;
+    const appointmentRequest = {
+        expiryPeriod,
+        stateUpdate: {
+            contractAddress: channelContract.address,
+            hashState,
+            round,
+            signatures: [sig0, sig1]
+        }
+    };
+
+    await request.post(`http://${config.host.name}:${config.host.port}/appointment`, { json: appointmentRequest });
+
+    // now register a callback on the setstate event and trigger a response
+    const setStateEvent = "EventEvidence(uint256, bytes32)";
+    let successResult = { success: false };
+    channelContract.on(setStateEvent, () => {
+        channelContract.removeAllListeners(setStateEvent);
+        successResult.success = true;
     });
 
+    // trigger a dispute
+    const tx = await channelContract.triggerDispute();
+    await tx.wait();
 
-    it("can create channel, submit appointment, trigger dispute, wait for response", async () => {
-        const round = 1,
-            setStateHash = KitsuneTools.hashForSetState(hashState, round, channelContract.address),
-            sig0 = await provider.getSigner(account0).signMessage(ethers.utils.arrayify(setStateHash)),
-            sig1 = await provider.getSigner(account1).signMessage(ethers.utils.arrayify(setStateHash)),
-            expiryPeriod = disputePeriod + 1;
-        const appointmentRequest = {
-            expiryPeriod,
-            stateUpdate: {
-                contractAddress: channelContract.address,
-                hashState,
-                round,
-                signatures: [sig0, sig1]
-            }
-        };
-
-        await request.post(`http://${config.host.name}:${config.host.port}/appointment`, { json: appointmentRequest });
-
-        // now register a callback on the setstate event and trigger a response
-        const setStateEvent = "EventEvidence(uint256, bytes32)";
-        let successResult = { success: false };
-        channelContract.on(setStateEvent, () => {
-            channelContract.removeAllListeners(setStateEvent);
-            successResult.success = true;
-        });
-
-        // trigger a dispute
-        const tx = await channelContract.triggerDispute();
-        await tx.wait();
-
-        try {
-            // wait for the success result
-            await waitForPredicate(successResult, s => s.success, 400);
-        } catch (doh) {
-            // fail if we dont get it
-            chai.assert.fail(true, false, "EventEvidence not successfully registered.");
-        }
-    }).timeout(3000);
-
-});
+    try {
+        // wait for the success result
+        await waitForPredicate(successResult, s => s.success, 400);
+    } catch (doh) {
+        // fail if we dont get it
+        console.error("TEST FAILED!!!");
+        console.log(doh);
+    }
+};
 
 // assess the value of a predicate after a timeout, throws if predicate does not evaluate to true
 const waitForPredicate = <T1>(successResult: T1, predicate: (a: T1) => boolean, timeout: number) => {
@@ -84,8 +75,13 @@ const waitForPredicate = <T1>(successResult: T1, predicate: (a: T1) => boolean, 
             if (predicate(successResult)) {
                 resolve();
             } else {
-                reject();
+                reject("Predicate not satisfied.");
             }
         }, timeout);
     });
 };
+
+console.log("Executing smoke tests");
+setup().then(() => {
+    execute().then(() => console.log("Tests passed!"));
+});
