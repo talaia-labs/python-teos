5162173c050fca48210fc723d200bb0df56c3aa8
Sergi Delgado <sergi.delgado.s@gmail.com>
2019-04-05 17:08:26 +0100

    Improves on watcher logic.
    Improves the dummy watcher logic. Sets a proper structure. Watcher can now check for potential matches and handle multiple appointments with the same locator (hash collisions).

--- pisa-btc/pisa/watcher.py
+++ pisa-btc/pisa/watcher.py
@@ -2,8 +2,10 @@ import zmq
 import binascii
 from queue import Queue
 from threading import Thread
+from pisa.tools import decrypt_tx
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-from conf import BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST, BTC_RPC_PORT, FEED_PROTOCOL, FEED_ADDR, FEED_PORT
+from conf import BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST, BTC_RPC_PORT, FEED_PROTOCOL, FEED_ADDR, FEED_PORT, \
+    MAX_APPOINTMENTS
 
 
 class ZMQHandler:
@@ -15,7 +17,7 @@ class ZMQHandler:
         self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "hashblock")
         self.zmqSubSocket.connect("%s://%s:%s" % (FEED_PROTOCOL, FEED_ADDR, FEED_PORT))
 
-    def handle(self, debug, block_queue):
+    def handle(self, block_queue, debug):
         msg = self.zmqSubSocket.recv_multipart()
         topic = msg[0]
         body = msg[1]
@@ -29,32 +31,50 @@ class ZMQHandler:
 
 
 class Watcher:
-    def __init__(self, debug):
-        self.appointments = []
-        self.sleep = True
-        self.debug = debug
+    def __init__(self, max_appointments=MAX_APPOINTMENTS):
+        self.appointments = dict()
         self.block_queue = Queue()
+        self.sleep = True
+        self.max_appointments = max_appointments
 
-    def add_appointment(self, appointment):
+    def add_appointment(self, appointment, debug):
         # ToDo: Discuss about validation of input data
-        self.appointments.append(appointment)
 
-        if self.sleep:
-            self.sleep = False
-            zmq_subscriber = Thread(target=self.do_subscribe, args=[self.block_queue])
-            zmq_subscriber.start()
-            self.do_watch()
+        if len(self.appointments) < self.max_appointments:
+            # Appointments are identified by the locator: the most significant 16 bytes of the commitment txid.
+            # While 16-byte hash collisions are not likely, they are possible, so we will store appointments in lists
+            # even if we only have one (so the code logic is simplified from this point on).
+            if not self.appointments.get(appointment.locator):
+                self.appointments[appointment.locator] = []
 
-        # Rationale:
-        # The Watcher will analyze every received block looking for appointment matches. If there is no work
-        # to do the watcher can sleep (appointments = [] and sleep = True) otherwise for every received block
-        # the watcher will get the list of transactions and compare it with the list of appointments
+            # Use an internal id (position in the list) to distinguish between different appointments with the same
+            # locator
+            appointment.id = len(self.appointments[appointment.locator])
+            self.appointments[appointment.locator].append(appointment)
 
-    def do_subscribe(self, block_queue):
+            if self.sleep:
+                self.sleep = False
+                zmq_subscriber = Thread(target=self.do_subscribe, args=[self.block_queue, debug])
+                watcher = Thread(target=self.do_watch, args=[debug])
+                zmq_subscriber.start()
+                watcher.start()
+
+            appointment_added = True
+
+            # Rationale:
+            # The Watcher will analyze every received block looking for appointment matches. If there is no work
+            # to do the watcher can sleep (appointments = [] and sleep = True) otherwise for every received block
+            # the watcher will get the list of transactions and compare it with the list of appointments
+        else:
+            appointment_added = False
+
+        return appointment_added
+
+    def do_subscribe(self, block_queue, debug):
         daemon = ZMQHandler()
-        daemon.handle(self.debug, block_queue)
+        daemon.handle(block_queue, debug)
 
-    def do_watch(self):
+    def do_watch(self, debug):
         bitcoin_cli = AuthServiceProxy("http://%s:%s@%s:%d" % (BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST,
                                                                BTC_RPC_PORT))
 
@@ -63,17 +83,51 @@ class Watcher:
 
             try:
                 block = bitcoin_cli.getblock(block_hash)
-
                 prev_block_id = block.get('previousblockhash')
                 txs = block.get('tx')
 
-                # ToDo: Check for every tx in txs if there is an appointment that matches (MS 16-bytes)
-
-                if self.debug:
-                    print("New block received ", block_hash)
-                    print("Prev. block hash ", prev_block_id)
+                if debug:
+                    print("New block received", block_hash)
+                    print("Prev. block hash", prev_block_id)
                     print("List of transactions", txs)
+
+                potential_matches = []
+
+                for k in self.appointments.keys():
+                    potential_matches += [(k, tx[32:]) for tx in txs if tx.startswith(k)]
+
+                if debug:
+                    if len(potential_matches) > 0:
+                        print("List of potential matches", potential_matches)
+                    else:
+                        print("No potential matches found")
+
+                matches = self.check_potential_matches(potential_matches, bitcoin_cli)
+
+                # ToDo: Handle matches
+                # ToDo: Matches will be empty list if no matches, list of matches otherwise
+                # ToDo: Notify responder with every match.
+                # ToDo: Get rid of appointment? Set appointment to a different state (create appointment state first)?
 
             except JSONRPCException as e:
                 print(e)
+                continue
+
+    def check_potential_matches(self, potential_matches, bitcoin_cli):
+        matches = []
+
+        for locator, k in potential_matches:
+            for appointment in self.appointments.get(locator):
+                try:
+                    decrypted_data = decrypt_tx(appointment.encrypted_blob, k, appointment.cypher)
+                    bitcoin_cli.decoderawtransaction(decrypted_data)
+                    matches.append((locator, appointment.id, decrypted_data))
+                except JSONRPCException as e:
+                    # Tx decode failed returns error code -22, maybe we should be more strict here. Leaving it simple
+                    # for the POC
+                    print(e)
+                    continue
+
+        return matches
+
 
