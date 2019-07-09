4563e2f420dfe54e47133f0d71541f45e52f5cd1
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2019-07-09 17:10:25 +0100

    Improves pisa-cli
    Big refactor of the cli, includes proper help messages and get_appointment command. Also creates a separate folder for the cli (apps can be separated by folders).

---
+++ pisa-btc/apps/cli/help.py
@@ -0,0 +1,21 @@
+def help_add_appointment():
+    return "NAME:" \
+        "\tpython pisa-cli add_appointment - Registers a json formatted appointment to the PISA server." \
+        "\n\nUSAGE:" \
+        "\tpython pisa-cli add_appointment [command options] appointment/path_to_appointment_file" \
+        "\n\nDESCRIPTION:" \
+        "\n\n\tRegisters a json formatted appointment to the PISA server." \
+        "\n\tif -f, --file *is* specified, the the command expects a path to a json file instead of a json encoded " \
+        "\n\tstring as parameter." \
+        "\n\nOPTIONS:" \
+        "\n\t -f, --file path_to_json_file\t loads the appointment data from the specified json file instead of" \
+        "\n\t\t\t\t\t command line"
+
+
+def help_get_appointment():
+    return "NAME:" \
+           "\tpython pisa-cli get_appointment - Gets json formatted data about an appointment from the PISA server." \
+           "\n\nUSAGE:" \
+           "\tpython pisa-cli get_appointment appointment_locator" \
+           "\n\nDESCRIPTION:" \
+           "\n\n\tGets json formatted data about an appointment from the PISA server.\n"
