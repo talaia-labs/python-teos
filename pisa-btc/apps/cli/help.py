40c73b87e39c2f70f9dbed54bf8f6b1b4c0c17ea
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2019-07-11 16:56:00 +0200

    Updates README and fixes typos

--- pisa-btc/apps/cli/help.py
+++ pisa-btc/apps/cli/help.py
@@ -5,7 +5,7 @@ def help_add_appointment():
         "\tpython pisa-cli add_appointment [command options] appointment/path_to_appointment_file" \
         "\n\nDESCRIPTION:" \
         "\n\n\tRegisters a json formatted appointment to the PISA server." \
-        "\n\tif -f, --file *is* specified, the the command expects a path to a json file instead of a json encoded " \
+        "\n\tif -f, --file *is* specified, then the command expects a path to a json file instead of a json encoded " \
         "\n\tstring as parameter." \
         "\n\nOPTIONS:" \
         "\n\t -f, --file path_to_json_file\t loads the appointment data from the specified json file instead of" \
