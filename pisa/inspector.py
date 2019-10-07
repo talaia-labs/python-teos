talaia-labs/python-teos

commit 93e23e769f1891cc3db010e2616be09980c714a1

Code clean up
Deletes debug/logging pair. Defines logging and bitcoin_cli as system-wide variables

pisa/inspector.py
@@ -1,16 +1,12 @@
 import re
-from pisa.appointment import Appointment
+import pisa.conf as conf
 from pisa import errors
-from pisa.utils.authproxy import AuthServiceProxy, JSONRPCException
-from pisa.conf import BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST, BTC_RPC_PORT, MIN_DISPUTE_DELTA, \
-    SUPPORTED_CIPHERS, SUPPORTED_HASH_FUNCTIONS
+from pisa import logging, bitcoin_cli
+from pisa.appointment import Appointment
+from pisa.utils.auth_proxy import JSONRPCException
 
 
 class Inspector:
-    def __init__(self, debug=False, logging=None):
-        self.debug = debug
-        self.logging = logging
-
     def inspect(self, data):
         locator = data.get('locator')
         start_time = data.get('start_time')
@@ -20,8 +16,6 @@ class Inspector:
         cipher = data.get('cipher')
         hash_function = data.get('hash_function')
 
-        bitcoin_cli = AuthServiceProxy("http://%s:%s@%s:%d" % (BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST,
-                                                               BTC_RPC_PORT))
         try:
             block_height = bitcoin_cli.getblockcount()
 
@@ -45,8 +39,7 @@ class Inspector:
                 r = (rcode, message)
 
         except JSONRPCException as e:
-            if self.debug:
-                self.logging.error("[Inspector] JSONRPCException. Error code {}".format(e))
+            logging.error("[Inspector] JSONRPCException. Error code {}".format(e))
 
             # In case of an unknown exception, assign a special rcode and reason.
             r = (errors.UNKNOWN_JSON_RPC_EXCEPTION, "Unexpected error occurred")
@@ -71,8 +64,7 @@ class Inspector:
             rcode = errors.APPOINTMENT_WRONG_FIELD_FORMAT
             message = "wrong locator format ({})".format(locator)
 
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
@@ -95,8 +87,7 @@ class Inspector:
             else:
                 message = "start_time too close to current height"
 
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
@@ -122,8 +113,7 @@ class Inspector:
             rcode = errors.APPOINTMENT_FIELD_TOO_SMALL
             message = 'end_time is in the past'
 
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
@@ -139,13 +129,12 @@ class Inspector:
         elif t != int:
             rcode = errors.APPOINTMENT_WRONG_FIELD_TYPE
             message = "wrong dispute_delta data type ({})".format(t)
-        elif dispute_delta < MIN_DISPUTE_DELTA:
+        elif dispute_delta < conf.MIN_DISPUTE_DELTA:
             rcode = errors.APPOINTMENT_FIELD_TOO_SMALL
             message = "dispute delta too small. The dispute delta should be at least {} (current: {})".format(
-                MIN_DISPUTE_DELTA, dispute_delta)
+                conf.MIN_DISPUTE_DELTA, dispute_delta)
 
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
@@ -166,8 +155,8 @@ class Inspector:
             # ToDo: #6 We may want to define this to be at least as long as one block of the cipher we are using
             rcode = errors.APPOINTMENT_WRONG_FIELD
             message = "wrong encrypted_blob"
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
@@ -183,12 +172,11 @@ class Inspector:
         elif t != str:
             rcode = errors.APPOINTMENT_WRONG_FIELD_TYPE
             message = "wrong cipher data type ({})".format(t)
-        elif cipher not in SUPPORTED_CIPHERS:
+        elif cipher not in conf.SUPPORTED_CIPHERS:
             rcode = errors.APPOINTMENT_CIPHER_NOT_SUPPORTED
             message = "cipher not supported: {}".format(cipher)
 
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
@@ -204,11 +192,10 @@ class Inspector:
         elif t != str:
             rcode = errors.APPOINTMENT_WRONG_FIELD_TYPE
             message = "wrong hash_function data type ({})".format(t)
-        elif hash_function not in SUPPORTED_HASH_FUNCTIONS:
+        elif hash_function not in conf.SUPPORTED_HASH_FUNCTIONS:
             rcode = errors.APPOINTMENT_HASH_FUNCTION_NOT_SUPPORTED
             message = "hash_function not supported {}".format(hash_function)
 
-        if self.debug and message:
-            self.logging.error("[Inspector] {}".format(message))
+        logging.error("[Inspector] {}".format(message))
 
         return rcode, message

pisa/__init__.py
@@ -1,2 +1,18 @@
+from pisa.utils.auth_proxy import AuthServiceProxy
+import pisa.conf as conf
+import logging
+
 HOST = 'localhost'
 PORT = 9814
+
+# Configure logging
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, handlers=[
+    logging.FileHandler(conf.SERVER_LOG_FILE),
+    logging.StreamHandler()
+])
+
+# Create RPC connection with bitcoind
+# TODO: Check if a long lived connection like this may create problems (timeouts)
+bitcoin_cli = AuthServiceProxy("http://%s:%s@%s:%d" % (conf.BTC_RPC_USER, conf.BTC_RPC_PASSWD, conf.BTC_RPC_HOST,
+                                                       conf.BTC_RPC_PORT))
+

pisa/api.py
@@ -1,14 +1,9 @@
-from pisa import *
+from pisa import HOST, PORT, logging, bitcoin_cli
 from pisa.watcher import Watcher
 from pisa.inspector import Inspector
 from pisa.appointment import Appointment
 from flask import Flask, request, Response, abort, jsonify
 import json
-
-
-# FIXME: HERE FOR TESTING (get_block_count). REMOVE WHEN REMOVING THE FUNCTION
-from pisa.utils.authproxy import AuthServiceProxy
-from pisa.conf import BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST, BTC_RPC_PORT
 
 # ToDo: #5-add-async-to-api
 app = Flask(__name__)
@@ -22,15 +17,14 @@ def add_appointment():
     remote_addr = request.environ.get('REMOTE_ADDR')
     remote_port = request.environ.get('REMOTE_PORT')
 
-    if debug:
-        logging.info('[API] connection accepted from {}:{}'.format(remote_addr, remote_port))
+    logging.info('[API] connection accepted from {}:{}'.format(remote_addr, remote_port))
 
     # Check content type once if properly defined
     request_data = json.loads(request.get_json())
     appointment = inspector.inspect(request_data)
 
     if type(appointment) == Appointment:
-        appointment_added = watcher.add_appointment(appointment, debug, logging)
+        appointment_added = watcher.add_appointment(appointment)
 
         # ToDo: #13-create-server-side-signature-receipt
         if appointment_added:
@@ -49,9 +43,7 @@ def add_appointment():
         rcode = HTTP_BAD_REQUEST
         response = "appointment rejected. Request does not match the standard"
 
-    if debug:
-        logging.info('[API] sending response and disconnecting: {} --> {}:{}'.format(response, remote_addr,
-                                                                                     remote_port))
+    logging.info('[API] sending response and disconnecting: {} --> {}:{}'.format(response, remote_addr, remote_port))
 
     return Response(response, status=rcode, mimetype='text/plain')
 
@@ -115,21 +107,16 @@ def get_all_appointments():
 
 @app.route('/get_block_count', methods=['GET'])
 def get_block_count():
-    bitcoin_cli = AuthServiceProxy("http://%s:%s@%s:%d" % (BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST,
-                                                           BTC_RPC_PORT))
-
     return jsonify({"block_count": bitcoin_cli.getblockcount()})
 
 
-def start_api(d, l):
+def start_api():
     # FIXME: Pretty ugly but I haven't found a proper way to pass it to add_appointment
-    global debug, logging, watcher, inspector
-    debug = d
-    logging = l
+    global watcher, inspector
 
     # ToDo: #18-separate-api-from-watcher
     watcher = Watcher()
-    inspector = Inspector(debug, logging)
+    inspector = Inspector()
 
     # Setting Flask log t ERROR only so it does not mess with out logging
     logging.getLogger('werkzeug').setLevel(logging.ERROR)

pisa/appointment.py
@@ -19,7 +19,7 @@ class Appointment:
 
         return appointment
 
-        # ToDO: #3-improve-appointment-strcuture
+        # ToDO: #3-improve-appointment-structure
 
 
 

pisa/pisad.py
@@ -1,36 +1,25 @@
-import logging
 from sys import argv
 from getopt import getopt
-from threading import Thread
+from pisa import logging
 from pisa.api import start_api
 from pisa.tools import can_connect_to_bitcoind, in_correct_network
-from pisa.utils.authproxy import AuthServiceProxy
-from pisa.conf import BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST, BTC_RPC_PORT, BTC_NETWORK, SERVER_LOG_FILE
 
 
 if __name__ == '__main__':
     debug = False
     opts, _ = getopt(argv[1:], 'd', ['debug'])
     for opt, arg in opts:
-        if opt in ['-d', '--debug']:
-            debug = True
+        # FIXME: Leaving this here for future option/arguments
+        pass
 
-    # Configure logging
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, handlers=[
-        logging.FileHandler(SERVER_LOG_FILE),
-        logging.StreamHandler()
-    ])
+    if can_connect_to_bitcoind():
+        if in_correct_network():
+            # Fire the api
+            start_api()
 
-    bitcoin_cli = AuthServiceProxy("http://%s:%s@%s:%d" % (BTC_RPC_USER, BTC_RPC_PASSWD, BTC_RPC_HOST,
-                                                           BTC_RPC_PORT))
-
-    if can_connect_to_bitcoind(bitcoin_cli):
-        if in_correct_network(bitcoin_cli, BTC_NETWORK):
-            # ToDo: This may not have to be a thead. The main thread only creates this and terminates.
-            api_thread = Thread(target=start_api, args=[debug, logging])
-            api_thread.start()
         else:
             logging.error("[Pisad] bitcoind is running on a different network, check conf.py and bitcoin.conf. "
                           "Shutting down")
+
     else:
         logging.error("[Pisad] can't connect to bitcoind. Shutting down")

commit 23efd48796931336eec63cb3b80e0c626ade1083

Fixes logging bug introduced in 93e23e7
When cleaning up code in commit 93e23e7 a condition was removed that makes the inspector log empty data.

pisa/inspector.py
@@ -47,7 +47,8 @@ class Inspector:
 
         return r
 
-    def check_locator(self, locator):
+    @staticmethod
+    def check_locator(locator):
         message = None
         rcode = 0
 
@@ -65,11 +66,13 @@ class Inspector:
             rcode = errors.APPOINTMENT_WRONG_FIELD_FORMAT
             message = "wrong locator format ({})".format(locator)
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
-    def check_start_time(self, start_time, block_height):
+    @staticmethod
+    def check_start_time(start_time, block_height):
         message = None
         rcode = 0
 
@@ -88,11 +91,13 @@ class Inspector:
             else:
                 message = "start_time too close to current height"
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
-    def check_end_time(self, end_time, start_time, block_height):
+    @staticmethod
+    def check_end_time(end_time, start_time, block_height):
         message = None
         rcode = 0
 
@@ -114,11 +119,13 @@ class Inspector:
             rcode = errors.APPOINTMENT_FIELD_TOO_SMALL
             message = 'end_time is in the past'
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
-    def check_delta(self, dispute_delta):
+    @staticmethod
+    def check_delta(dispute_delta):
         message = None
         rcode = 0
 
@@ -135,12 +142,14 @@ class Inspector:
             message = "dispute delta too small. The dispute delta should be at least {} (current: {})".format(
                 conf.MIN_DISPUTE_DELTA, dispute_delta)
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
     # ToDo: #6-define-checks-encrypted-blob
-    def check_blob(self, encrypted_blob):
+    @staticmethod
+    def check_blob(encrypted_blob):
         message = None
         rcode = 0
 
@@ -157,11 +166,13 @@ class Inspector:
             rcode = errors.APPOINTMENT_WRONG_FIELD
             message = "wrong encrypted_blob"
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
-    def check_cipher(self, cipher):
+    @staticmethod
+    def check_cipher(cipher):
         message = None
         rcode = 0
 
@@ -177,11 +188,13 @@ class Inspector:
             rcode = errors.APPOINTMENT_CIPHER_NOT_SUPPORTED
             message = "cipher not supported: {}".format(cipher)
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message
 
-    def check_hash_function(self, hash_function):
+    @staticmethod
+    def check_hash_function(hash_function):
         message = None
         rcode = 0
 
@@ -197,6 +210,7 @@ class Inspector:
             rcode = errors.APPOINTMENT_HASH_FUNCTION_NOT_SUPPORTED
             message = "hash_function not supported {}".format(hash_function)
 
-        logging.error("[Inspector] {}".format(message))
+        if message is not None:
+            logging.error("[Inspector] {}".format(message))
 
         return rcode, message