41453bd2716a094d9b294e930be74f455159e008
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-10-11 12:18:11 +0700

    Updated sample_conf

--- pisa/sample_conf.py
+++ pisa/sample_conf.py
@@ -17,7 +17,7 @@ EXPIRY_DELTA = 6
 MIN_DISPUTE_DELTA = 20
 SERVER_LOG_FILE = 'pisa.log'
 DB_PATH = 'appointments/'
-SIGNING_KEY_DER = None
+SIGNING_KEY_FILE = None
 
 # PISA-CLI
 CLIENT_LOG_FILE = 'pisa.log'
