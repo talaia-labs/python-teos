3d4ed28c8bc6decd8b0481a2e96fe5e4401151ad
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-01-15 15:41:57 +0100

    Fixes wrong log messase

--- apps/cli/blob.py
+++ apps/cli/blob.py
@@ -4,6 +4,6 @@ import re
 class Blob:
     def __init__(self, data):
         if type(data) is not str or re.search(r"^[0-9A-Fa-f]+$", data) is None:
-            raise ValueError("Non-Hex character found in txid.")
+            raise ValueError("Non-Hex character found in transaction.")
 
         self.data = data
