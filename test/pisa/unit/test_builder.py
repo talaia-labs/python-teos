a718a5c6ae1c2815517d5175ea6024ec657d4161
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-01-24 13:32:20 +0100

    Updates Builder unit tests

--- test/pisa/unit/test_builder.py
+++ test/pisa/unit/test_builder.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from queue import Queue
 
 from pisa.builder import Builder
 from test.pisa.unit.conftest import get_random_value_hex, generate_dummy_appointment, generate_dummy_tracker
@@ -29,8 +30,9 @@ def test_build_appointments():
     # Check that the created appointments match the data
     for uuid, appointment in appointments.items():
         assert uuid in appointments_data.keys()
-        assert appointments_data[uuid] == appointment.to_dict()
-        assert uuid in locator_uuid_map[appointment.locator]
+        assert appointments_data[uuid].get("locator") == appointment.get("locator")
+        assert appointments_data[uuid].get("end_time") == appointment.get("end_time")
+        assert uuid in locator_uuid_map[appointment.get("locator")]
 
 
 def test_build_trackers():
@@ -55,17 +57,18 @@ def test_build_trackers():
     # Check that the built trackers match the data
     for uuid, tracker in trackers.items():
         assert uuid in trackers_data.keys()
-        tracker_dict = tracker.to_dict()
 
-        # The locator is not part of the tracker_data found in the database (for now)
-        assert trackers_data[uuid] == tracker_dict
-        assert uuid in tx_tracker_map[tracker.penalty_txid]
+        assert tracker.get("penalty_txid") == trackers_data[uuid].get("penalty_txid")
+        assert tracker.get("locator") == trackers_data[uuid].get("locator")
+        assert tracker.get("appointment_end") == trackers_data[uuid].get("appointment_end")
+        assert uuid in tx_tracker_map[tracker.get("penalty_txid")]
 
 
-def test_build_block_queue():
+def test_populate_block_queue():
     # Create some random block hashes and construct the queue with them
     blocks = [get_random_value_hex(32) for _ in range(10)]
-    queue = Builder.build_block_queue(blocks)
+    queue = Queue()
+    Builder.populate_block_queue(queue, blocks)
 
     # Make sure every block is in the queue and that there are not additional ones
     while not queue.empty():
