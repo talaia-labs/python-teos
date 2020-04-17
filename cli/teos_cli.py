orbitalturtle/pisa_btc

Add get_all_appointments cli command

cli/teos_cli.py
@@ -11,7 +11,7 @@ from getopt import getopt, GetoptError
 from requests import ConnectTimeout, ConnectionError
 from requests.exceptions import MissingSchema, InvalidSchema, InvalidURL
 
-from cli.help import show_usage, help_add_appointment, help_get_appointment
+from cli.help import show_usage, help_add_appointment, help_get_appointment, help_get_all_appointments
 from cli import DEFAULT_CONF, DATA_DIR, CONF_FILE_NAME, LOG_PREFIX
 
 import common.cryptographer
@@ -351,7 +351,7 @@ def get_appointment(locator, get_appointment_endpoint):
         get_appointment_endpoint (:obj:`str`): the teos endpoint where to get appointments from.
 
     Returns:
-        :obj:`dict` or :obj:`None`: a dictionary containing thew appointment data if the locator is valid and the tower
+        :obj:`dict` or :obj:`None`: a dictionary containing the appointment data if the locator is valid and the tower
         responds. ``None`` otherwise.
     """
 
@@ -377,6 +377,34 @@ def get_appointment(locator, get_appointment_endpoint):
 
     except requests.exceptions.InvalidSchema:
         logger.error("No transport protocol found. Have you missed http(s):// in the server url?")
+
+    except requests.exceptions.Timeout:
+        logger.error("The request timed out")
+
+
+def get_all_appointments(get_all_appointments_endpoint):
+    """ 
+    Gets information about all appointments stored in the tower, if the user requesting the data is an administrator.
+
+    Args:
+        get_all_appointments_endpoint (:obj:`str`): the teos endpoint from which all appointments can be retrieved.
+
+    Returns:
+        :obj:`dict` a dictionary containing the all the appointments stored by the responder and watcher if the tower
+        responds. 
+    """
+
+    try:
+        r = requests.get(url=get_all_appointments_endpoint, timeout=5)
+        return r.json()
+
+    except ConnectTimeout:
+        logger.error("Can't connect to the Eye of Satoshi's API. Connection timeout")
+        return None
+
+    except ConnectionError:
+        logger.error("Can't connect to the Eye of Satoshi's API. Server cannot be reached")
+        return None
 
     except requests.exceptions.Timeout:
         logger.error("The request timed out")
@@ -419,6 +447,12 @@ def main(args, command_line_conf):
                         if appointment_data:
                             print(appointment_data)
 
+                elif command == "get_all_appointments":
+                    get_all_appointments_endpoint = "{}/get_all_appointments".format(teos_url)
+                    appointment_data = get_all_appointments(get_all_appointments_endpoint)
+                    if appointment_data:
+                        print(appointment_data)
+
                 elif command == "help":
                     if args:
                         command = args.pop(0)
@@ -428,6 +462,9 @@ def main(args, command_line_conf):
 
                         elif command == "get_appointment":
                             sys.exit(help_get_appointment())
+
+                        elif command == "get_all_appointments":
+                            sys.exit(help_get_all_appointments())
 
                         else:
                             logger.error("Unknown command. Use help to check the list of available commands")
@@ -447,7 +484,7 @@ def main(args, command_line_conf):
 
 if __name__ == "__main__":
     command_line_conf = {}
-    commands = ["add_appointment", "get_appointment", "help"]
+    commands = ["add_appointment", "get_appointment", "get_all_appointments", "help"]
 
     try:
         opts, args = getopt(argv[1:], "s:p:h", ["server", "port", "help"])

test/cli/unit/test_teos_cli.py
@@ -3,7 +3,7 @@ import json
 import shutil
 import responses
 from coincurve import PrivateKey
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, Timeout
 
 import common.cryptographer
 from common.logger import Logger
@@ -240,3 +240,37 @@ def test_get_appointment_err():
     responses.add(responses.GET, request_url, body=ConnectionError())
 
     assert not teos_cli.get_appointment(locator, get_appointment_endpoint)
+
+
+@responses.activate
+def test_get_all_appointments():
+    # Response of get_appointment endpoint is an appointment with status added to it.
+    dummy_appointment_full["status"] = "being_watched"
+    response = dummy_appointment_full
+    get_all_appointments_endpoint = teos_endpoint + "get_all_appointments"
+
+    request_url = get_all_appointments_endpoint
+    responses.add(responses.GET, request_url, json=response, status=200)
+    result = teos_cli.get_all_appointments(get_all_appointments_endpoint)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == request_url
+    assert result.get("locator") == response.get("locator")
+
+
+@responses.activate
+def test_get_all_appointments_err():
+    locator = get_random_value_hex(16)
+    get_all_appointments_endpoint = teos_endpoint + "get_all_appointments"
+
+    # Test that get_all_appointments handles a connection error appropriately.
+    request_url = get_all_appointments_endpoint
+    responses.add(responses.GET, request_url, body=ConnectionError())
+
+    assert not teos_cli.get_all_appointments(get_all_appointments_endpoint)
+
+    # Test that get_all_appointments handles a timeout error appropriately.
+    request_url = get_all_appointments_endpoint
+    responses.add(responses.GET, request_url, body=Timeout())
+
+    assert not teos_cli.get_all_appointments(get_all_appointments_endpoint)

talaia-labs/python-teos

cli/teos_cli.py
@@ -10,7 +10,7 @@ from getopt import getopt, GetoptError
 from requests import ConnectTimeout, ConnectionError
 from requests.exceptions import MissingSchema, InvalidSchema, InvalidURL
 
-from cli.help import show_usage, help_add_appointment, help_get_appointment, help_register
+from cli.help import show_usage, help_add_appointment, help_get_appointment, help_register, help_get_all_appointments
 from cli import DEFAULT_CONF, DATA_DIR, CONF_FILE_NAME, LOG_PREFIX
 
 import common.cryptographer
@@ -173,6 +173,39 @@ def get_appointment(locator, cli_sk, teos_pk, teos_url):
     response_json = process_post_response(server_response)
 
     return response_json
+
+
+def get_all_appointments(teos_url):
+    """ 
+    Gets information about all appointments stored in the tower, if the user requesting the data is an administrator.
+
+    Args:
+        get_all_appointments_endpoint (:obj:`str`): the teos endpoint from which all appointments can be retrieved.
+
+    Returns:
+        :obj:`dict` a dictionary containing all the appointments stored by the Responder and Watcher if the tower
+        responds. 
+    """
+
+    get_all_appointments_endpoint = "{}/get_all_appointments".format(teos_url)
+
+    try:
+        response = requests.get(url=get_all_appointments_endpoint, timeout=5)
+
+        if response.status_code != constants.HTTP_OK:
+            logger.error("The server returned a 'not found' error")
+            return None
+
+        response_json = json.dumps(response.json(), indent=4, sort_keys=True)
+        return response_json
+
+    except ConnectionError:
+        logger.error("Can't connect to the Eye of Satoshi's API. Server cannot be reached")
+        return None
+
+    except requests.exceptions.Timeout:
+        logger.error("The request timed out")
+        return None
 
 
 def load_keys(teos_pk_path, cli_sk_path, cli_pk_path):
@@ -426,6 +459,11 @@ def main(args, command_line_conf):
                             if appointment_data:
                                 print(appointment_data)
 
+                    elif command == "get_all_appointments":
+                        appointment_data = get_all_appointments(teos_url)
+                        if appointment_data:
+                            print(appointment_data)
+
                     elif command == "help":
                         if args:
                             command = args.pop(0)
@@ -442,6 +480,9 @@ def main(args, command_line_conf):
                             else:
                                 logger.error("Unknown command. Use help to check the list of available commands")
 
+                        elif command == "get_all_appointments":
+                            sys.exit(help_get_all_appointments())
+
                         else:
                             sys.exit(show_usage())
 
@@ -457,7 +498,7 @@ def main(args, command_line_conf):
 
 if __name__ == "__main__":
     command_line_conf = {}
-    commands = ["register", "add_appointment", "get_appointment", "help"]
+    commands = ["register", "add_appointment", "get_appointment", "get_all_appointments", "help"]
 
     try:
         opts, args = getopt(argv[1:], "s:p:h", ["server", "port", "help"])

test/cli/unit/test_teos_cli.py
@@ -4,7 +4,8 @@ import shutil
 import responses
 from binascii import hexlify
 from coincurve import PrivateKey
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, Timeout
+from http.client import HTTPException
 
 import common.cryptographer
 from common.logger import Logger
@@ -31,6 +32,7 @@ teos_url = "http://{}:{}".format(config.get("TEOS_SERVER"), config.get("TEOS_POR
 add_appointment_endpoint = "{}/add_appointment".format(teos_url)
 register_endpoint = "{}/register".format(teos_url)
 get_appointment_endpoint = "{}/get_appointment".format(teos_url)
+get_all_appointments_endpoint = "{}/get_all_appointments".format(teos_url)
 
 dummy_appointment_data = {
     "tx": get_random_value_hex(192),
@@ -194,7 +196,7 @@ def test_post_request():
 
 @responses.activate
 def test_process_post_response():
-    # Let's first crete a response
+    # Let's first create a response
     response = {
         "locator": dummy_appointment.to_dict()["locator"],
         "signature": get_signature(dummy_appointment.serialize(), dummy_teos_sk),
@@ -257,3 +259,37 @@ def test_save_appointment_receipt(monkeypatch):
     assert any([dummy_appointment.locator in f for f in files])
 
     shutil.rmtree(appointments_folder)
+
+
+@responses.activate
+def test_get_all_appointments():
+    # Response of get_all_appointments endpoint is all appointments from watcher and responder.
+    dummy_appointment_dict["status"] = "being_watched"
+    response = {"watcher_appointments": dummy_appointment_dict, "responder_trackers": {}}
+
+    request_url = get_all_appointments_endpoint
+    responses.add(responses.GET, request_url, json=response, status=200)
+    result = teos_cli.get_all_appointments(teos_url)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == request_url
+    assert json.loads(result).get("locator") == response.get("locator")
+
+
+@responses.activate
+def test_get_all_appointments_err():
+    # Test that get_all_appointments handles a connection error appropriately.
+    request_url = get_all_appointments_endpoint
+    responses.add(responses.GET, request_url, body=ConnectionError())
+
+    assert not teos_cli.get_all_appointments(teos_url)
+
+    # Test that get_all_appointments handles a timeout error appropriately.
+    responses.replace(responses.GET, request_url, body=Timeout())
+
+    assert not teos_cli.get_all_appointments(teos_url)
+
+    # Test that get_all_appointments handles a 404 error appropriately.
+    responses.replace(responses.GET, request_url, status=404)
+
+    assert teos_cli.get_all_appointments(teos_url) is None