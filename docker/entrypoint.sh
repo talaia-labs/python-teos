7da2933c48cf6ff60d5ce515971289cc19d08ea7
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-06-08 23:28:50 +0200

    Move files to docker/ and fixes Dockerfile and entrypoint

---
+++ docker/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+START_COMMAND="/usr/local/bin/python3 -m teos.teosd "
+
+if [[ ! -z ${API_BIND} ]]; then
+    START_COMMAND=$START_COMMAND" --apibind=""$API_BIND"
+fi
+
+if [[ ! -z ${API_PORT} ]]; then
+    START_COMMAND=$START_COMMAND" --apiport=""$API_PORT"
+fi
+
+if [[ ! -z ${BTC_NETWORK} ]]; then
+    START_COMMAND=$START_COMMAND" --btcnetwork=""$BTC_NETWORK"
+fi
+
+if [[ ! -z ${BTC_RPC_USER} ]]; then 
+    START_COMMAND=$START_COMMAND" --btcrpcuser=""$BTC_RPC_USER"
+fi
+
+if [[ ! -z ${BTC_RPC_PASSWORD} ]]; then
+    START_COMMAND=$START_COMMAND" --btcrpcpassword=""$BTC_RPC_PASSWORD"
+fi
+
+if [[ ! -z ${BTC_RPC_CONNECT} ]]; then
+    START_COMMAND=$START_COMMAND" --btcrpcconnect=""$BTC_RPC_CONNECT"
+fi
+
+if [[ ! -z ${BTC_RPC_PORT} ]]; then
+    START_COMMAND=$START_COMMAND" --btcrpcport=""$BTC_RPC_PORT"
+fi
+
+$START_COMMAND
