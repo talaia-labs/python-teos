57ff453fe387ff2e174a70d68200aaebfab73a6f
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2020-07-10 13:03:45 +0200

    Added tests for CustomLogRenderer

---
+++ test/common/unit/test_logger.py
@@ -0,0 +1,44 @@
+import os
+import logging
+
+from common.constants import LOCATOR_LEN_BYTES
+from common.logger import get_logger, setup_logging, CustomLogRenderer
+
+
+def test_CustomLogRenderer_with_event():
+    event_dict = {
+        "event": "Test",
+    }
+    renderer = CustomLogRenderer()
+    assert renderer(None, None, event_dict) == "Test"
+
+
+def test_CustomLogRenderer_with_event_and_timestamp():
+    event_dict = {
+        "event": "Test",
+        "timestamp": "today",  # doesn't matter if it's not correct, should just copy it verbatim
+    }
+    renderer = CustomLogRenderer()
+    assert renderer(None, None, event_dict) == "today Test"
+
+
+def test_CustomLogRenderer_with_event_and_timestamp_and_component():
+    event_dict = {
+        "component": "MyAwesomeComponent",
+        "event": "Test",
+        "timestamp": "today",  # doesn't matter if it's not correct, should just copy it verbatim
+    }
+    renderer = CustomLogRenderer()
+    assert renderer(None, None, event_dict) == "today [MyAwesomeComponent] Test"
+
+
+def test_CustomLogRenderer_with_event_and_timestamp_and_component_and_extra_keys():
+    event_dict = {
+        "component": "MyAwesomeComponent",
+        "event": "Test",
+        "timestamp": "today",  # doesn't matter if it's not correct, should just copy it verbatim
+        "key": 6,
+        "aKeyBefore": 42,  # should be rendered before "key", because it comes lexicographically before
+    }
+    renderer = CustomLogRenderer()
+    assert renderer(None, None, event_dict) == "today [MyAwesomeComponent] Test\taKeyBefore=42 key=6"
