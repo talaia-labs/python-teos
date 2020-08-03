00394362751091eb6945ebed70bdeedbd08df6c3
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-08-03 20:33:59 +0200

    test - fix e2e*
    There's an issue with the current version of the e2e tests not killingthe gunicorn process when the main process is being killed. I think this can be solved by shuting down the daemon with the rpc command instead of a terminate, but that has to be done in a follow-up.

--- test/teos/e2e/test_basic_e2e.py
+++ test/teos/e2e/test_basic_e2e.py
@@ -15,6 +15,7 @@ from common.tools import compute_locator
 from common.appointment import Appointment
 from common.cryptographer import Cryptographer
 
+import teos.cli.teos_cli as teos_cli
 from teos.utils.auth_proxy import JSONRPCException
 
 from test.teos.conftest import (
@@ -32,7 +33,6 @@ teos_base_endpoint = "http://{}:{}".format(config.get("API_BIND"), config.get("A
 teos_add_appointment_endpoint = "{}/add_appointment".format(teos_base_endpoint)
 teos_get_appointment_endpoint = "{}/get_appointment".format(teos_base_endpoint)
 teos_get_all_appointments_endpoint = "{}/get_all_appointments".format(teos_base_endpoint)
-teos_rpc_endpoint = "http://{}:{}/rpc".format(config.get("RPC_BIND"), config.get("RPC_PORT"))
 
 
 user_sk = Cryptographer.generate_key()
@@ -59,12 +59,6 @@ def make_rpc_request(rpc_url, method, *args):
     return requests.post(
         url=rpc_url, json={"method": method, "params": args, "jsonrpc": "2.0", "id": uuid4().int}, timeout=5
     )
-
-
-def get_all_appointments():
-    response = make_rpc_request(teos_rpc_endpoint, "get_all_appointments")
-    jsonrpc_response = response.json()
-    return jsonrpc_response["result"]
 
 
 def test_commands_non_registered(teosd):
@@ -126,7 +120,7 @@ def test_appointment_life_cycle():
     assert appointment_info.get("appointment") == appointment.to_dict()
 
     # Check also the get_all_appointment endpoint
-    all_appointments = get_all_appointments()
+    all_appointments = teos_cli.get_all_appointments(config.get("RPC_BIND"), config.get("RPC_PORT"))
     watching = all_appointments.get("watcher_appointments")
     responding = all_appointments.get("responder_trackers")
     assert len(watching) == appointments_in_watcher and len(responding) == 0
@@ -139,7 +133,7 @@ def test_appointment_life_cycle():
     appointments_in_watcher -= 1
     appointments_in_responder += 1
 
-    all_appointments = get_all_appointments()
+    all_appointments = teos_cli.get_all_appointments(config.get("RPC_BIND"), config.get("RPC_PORT"))
     watching = all_appointments.get("watcher_appointments")
     responding = all_appointments.get("responder_trackers")
     assert len(watching) == appointments_in_watcher and len(responding) == appointments_in_responder
@@ -210,7 +204,7 @@ def test_multiple_appointments_life_cycle():
         sleep(1)
 
     # Test that they all show up in get_all_appointments at the correct stages.
-    all_appointments = get_all_appointments()
+    all_appointments = teos_cli.get_all_appointments(config.get("RPC_BIND"), config.get("RPC_PORT"))
     watching = all_appointments.get("watcher_appointments")
     responding = all_appointments.get("responder_trackers")
     assert len(watching) == appointments_in_watcher and len(responding) == appointments_in_responder
