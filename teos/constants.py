f5f03ad208c9941518bc50643049c500aa908937
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2020-09-14 13:11:20 +0200

    Added config params for the internal api host, port and number of workers

--- teos/constants.py
+++ teos/constants.py
@@ -1,6 +1,2 @@
-INTERNAL_API_HOST = "localhost"
-INTERNAL_API_PORT = "50051"
-INTERNAL_API_ENDPOINT = f"{INTERNAL_API_HOST}:{INTERNAL_API_PORT}"
-
 # the grace time in seconds to complete any pending call when stopping one of the services of TEOS
 SHUTDOWN_GRACE_TIME = 10
