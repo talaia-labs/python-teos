40399e410dff011a9268724d34e3c09f42bf9674
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-10-16 12:49:29 +0200

    teos: Adds formatting for waitress
    Merges as much as posible in teos.logger so we don't have to replicate code in gunicorn_config

--- teos/gunicorn_config.py
+++ teos/gunicorn_config.py
@@ -1,43 +1,9 @@
 import os
 import structlog
-from logging.handlers import SocketHandler
+from teos.logger import add_api_component, timestamper
 
 
 logging_port = os.environ.get("LOG_SERVER_PORT")
-timestamper = structlog.processors.TimeStamper(fmt="%d/%m/%Y %H:%M:%S")
-
-
-class FormattedSocketHandler(SocketHandler):
-    """ Works in the same way as SocketHandler, but it uses formatters. """
-
-    def emit(self, record):
-        """
-        Works exactly like SocketHandler.emit but formats the record before sending it.
-        record.args is set to none since they are already used by self.format, otherwise makePickle would try to
-        use them again and fail.
-
-        Args:
-            record (:obj:LogRecord <logging.LogRecord>): the record to be emitted.
-        """
-        try:
-            record.msg = self.format(record)
-            record.args = None
-
-            s = self.makePickle(record)
-            self.send(s)
-
-        except Exception:
-            self.handleError(record)
-
-
-def add_component(logger, name, event_dict):
-    """ Adds the component name to the structlog."""
-    event_dict["component"] = "API"
-    return event_dict
-
-
-# Add the timestamp and component to the entry if the entry is not from structlog.
-pre_chain = [timestamper, add_component]
 
 
 # Config dict that will be used by gunicorn
@@ -62,20 +28,20 @@ logconfig_dict = {
         "json_formatter": {
             "()": structlog.stdlib.ProcessorFormatter,
             "processor": structlog.processors.JSONRenderer(),
-            "foreign_pre_chain": pre_chain,
+            "foreign_pre_chain": [timestamper, add_api_component],
         }
     },
     "handlers": {
         "error_console": {
             "level": "DEBUG",
-            "class": "teos.gunicorn_config.FormattedSocketHandler",
+            "class": "teos.logger.FormattedSocketHandler",
             "host": "localhost",
             "port": logging_port,
             "formatter": "json_formatter",
         },
         "console": {
             "level": "DEBUG",
-            "class": "teos.gunicorn_config.FormattedSocketHandler",
+            "class": "teos.logger.FormattedSocketHandler",
             "host": "localhost",
             "port": logging_port,
             "formatter": "json_formatter",
